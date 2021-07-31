6891fc1ab80db887808eca257dac2cdc95bc22b7
MohamedBenIsmaiel <generalbuismaiel@gmail.com>
2021-08-01 01:43:04 +0200

    update createUser function by using Validate method

--- service/users/users_service.go
+++ service/users/users_service.go
@@ -2,16 +2,11 @@ package UsersService
 import(
 	"github.com/MohamedBenIsmaiel/bookStore-userApi.git/domain/users"
 	"github.com/MohamedBenIsmaiel/bookStore-userApi.git/util/error"
-	"net/http"
 )
 
 func CreateUser(user UserDomain.User)(*UserDomain.User, *Error.RestError){
-	if len(user.FirstName) < 1  || len(user.Email) < 1  || len(user.LastName) < 1 {
-		return nil , &Error.RestError{
-			Message: "Missing required Fields",
-			Code: http.StatusBadRequest,
-			Error:"BadRequest",
-		}
+	if err:= user.Validate(); err != nil{
+		return nil, err
 	}
 	return &user, nil
 }
