9eda3a919f3862829045963d8b600678729111c2
MohamedBenIsmaiel <generalbuismaiel@gmail.com>
2021-08-01 01:28:35 +0200

    adding validation function

---
+++ domain/users/dta.go
@@ -0,0 +1,22 @@
+package UserDomain
+import(
+	"strings"
+	"github.com/MohamedBenIsmaiel/bookStore-userApi.git/util/error"
+)
+
+type User struct{
+	Id int64	  `json:"id"`
+	FirstName string  `json:"firstName"`
+	LastName string   `json:"lastName"`
+	Email string	  `json:"email"`
+	Password string   `json:"password"`
+}
+
+func (user User)Validate() *Error.RestError{
+	user.Email = strings.TrimSpace(strings.ToLower(user.Email))
+	if user.Email == ""{
+		err :=  Error.NewBadRequest("Invalid Email")
+		return &err
+	}
+	return nil
+}
